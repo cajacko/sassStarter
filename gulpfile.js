92f61da41816c5f72bdde048641a0d17cfcebac1
Charlie Jackson <charlie@charliejackson.com>
2016-04-21 17:45:33 +0100

    Added common components

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
+/**
+ * Gulp sass is used here to test if the sass is compiling 
+ * correctly. After 'npm install' run 'gulp' to check.
+ */
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-/********************************************************
-* SASS                                                  *
-********************************************************/
 gulp.task('default', function() {
-    return gulp.src('./sass/import.scss')
+    return gulp.src('./scss/import.scss')
         .pipe(sass().on('error', sass.logError));
 });
